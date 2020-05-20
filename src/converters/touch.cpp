c12f920edd7a850a7d0f3f2d908c76cfc6f58958
mbusy <mbusy.pro@gmail.com>
2020-05-20 17:45:32 +0200

    Refactor the touch converter

--- src/converters/touch.cpp
+++ src/converters/touch.cpp
@@ -62,9 +62,9 @@ void TouchEventConverter<T>::callAll(const std::vector<message_actions::MessageA
 }
 
 // http://stackoverflow.com/questions/8752837/undefined-reference-to-template-class-constructor
-template class TouchEventConverter<naoqi_bridge_msgs::Bumper>;
-template class TouchEventConverter<naoqi_bridge_msgs::HandTouch>;
-template class TouchEventConverter<naoqi_bridge_msgs::HeadTouch>;
+template class TouchEventConverter<naoqi_bridge_msgs::msg::Bumper>;
+template class TouchEventConverter<naoqi_bridge_msgs::msg::HandTouch>;
+template class TouchEventConverter<naoqi_bridge_msgs::msg::HeadTouch>;
 }
 
 }
